75d42fe0c40cf9ce9ba6075fec8e39510a19d117
Tom Robinson <iamtmrobinson@users.noreply.github.com>
2018-12-14 17:22:32 +0000

    Working force powered from state

--- src/components/Graph/index.js
+++ src/components/Graph/index.js
@@ -4,34 +4,57 @@ import * as d3 from "d3";
 const WIDTH = 300;
 const HEIGHT = 300;
 
-const initialise = svgElement => {
-  const svg = d3
-    .select(svgElement)
-    .attr("width", WIDTH)
-    .attr("height", HEIGHT);
-};
-
-const draw = (svgElement, props) => {
-  const svg = d3.select(svgElement);
-
-  svg
-    .append("text")
-    .text("Edit the code below to change me!")
-    .attr("x", WIDTH / 2)
-    .attr("y", HEIGHT / 2)
-    .attr("font-size", 12)
-    .attr("font-family", "monospace");
-};
-
 class Graph extends React.Component {
   componentDidMount() {
-    initialise(this.svg);
-    draw(this.svg, this.props);
+    this.initialise();
+    this.draw(this.props);
   }
 
   componentDidUpdate() {
-    draw(this.svg, this.props);
+    this.draw(this.props);
   }
+
+  initialise = () => {
+    const svg = d3
+      .select(this.svg)
+      .attr("width", WIDTH)
+      .attr("height", HEIGHT);
+
+    svg
+      .append("g")
+      .attr("class", "container")
+      .attr("transform", "translate(" + WIDTH / 2 + "," + HEIGHT / 2 + ")");
+  };
+
+  draw = props => {
+    const svg = d3.select(".container");
+
+    var simulation = d3
+      .forceSimulation(props.graph.nodes)
+      .force("charge", d3.forceManyBody())
+      .force("center", d3.forceCenter())
+      // .alphaTarget(1)
+      .on("tick", this.ticked);
+
+    this.nodesSelection = svg.selectAll(".node").data(props.graph.nodes);
+
+    this.nodesSelection
+      .enter()
+      .append("circle")
+      .attr("class", "node")
+      .style("fill", "#45b29d")
+      .attr("r", 5);
+  };
+
+  ticked = () => {
+    this.nodesSelection
+      .attr("cx", function(d) {
+        return d.x;
+      })
+      .attr("cy", function(d) {
+        return d.y;
+      });
+  };
 
   render() {
     const { graph } = this.props;
