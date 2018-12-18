96bed165d1ce6a8288d0babb04c2fd21321dce68
Tom Robinson <iamtmrobinson@users.noreply.github.com>
2018-12-18 18:16:16 +0000

    Nodes update correctly

--- src/components/Graph/index.js
+++ src/components/Graph/index.js
@@ -4,43 +4,31 @@ import * as d3 from "d3";
 const WIDTH = 500;
 const HEIGHT = 500;
 
-const clamp = (value, min, max) => {
-  return Math.min(Math.max(value, min), max);
-};
-
 class Graph extends React.Component {
   componentDidMount() {
-    console.log("Did Mount");
-
     this.initialise(this.props);
     this.draw(this.props);
   }
 
-  shouldComponentUpdate(nextProps) {
-    console.log("Should Update");
-
-    this.draw(nextProps);
-    return true;
+  componentDidUpdate() {
+    this.draw(this.props);
   }
 
-  componentDidUpdate() {}
-
   initialise = props => {
-    console.log("Initialise");
-
-    const svg = d3
+    const container = d3
       .select(this.svg)
       .attr("width", WIDTH)
-      .attr("height", HEIGHT);
-
-    svg
+      .attr("height", HEIGHT)
       .append("g")
       .attr("class", "container")
       .attr("transform", "translate(" + WIDTH / 2 + "," + HEIGHT / 2 + ")");
 
+    container.append("g").attr("class", "nodes");
+    container.append("g").attr("class", "links");
+
     this.simulation = d3
       .forceSimulation(props.graph.nodes, props.graph.links)
-      .alphaDecay(0.5)
+      .alphaDecay(0.2)
       .force("charge", d3.forceManyBody())
       .force("link", d3.forceLink(props.graph.links).id(d => d.id))
       .force("collide", d3.forceCollide().strength(1))
@@ -48,65 +36,30 @@ class Graph extends React.Component {
   };
 
   draw = props => {
-    console.log("Draw", props.graph.links);
+    this.nodesSelection = d3
+      .select(".nodes")
+      .selectAll(".node")
+      .data(props.graph.nodes);
 
-    const svg = d3.select(".container");
-
-    this.nodesSelection = svg.selectAll(".node").data(props.graph.nodes);
-
-    const nodeTransition = d3.transition().duration(250);
-
-    this.nodesSelection.exit().remove();
-
-    var nodeContainers = this.nodesSelection
+    this.nodesSelection = this.nodesSelection
       .enter()
-      .append("g")
-      .attr("class", "node")
-      .merge(this.nodesSelection);
-
-    nodeContainers
       .append("circle")
-      .style("fill", "#45b29d")
-      .attr("r", 5);
-
-    nodeContainers.append("text").text(d => d.id);
-
-    this.linksSelection = svg.selectAll(".link").data(props.graph.links);
-
-    this.linksSelection
-      .exit()
-      .style("fill", "#b26745")
-      .transition(nodeTransition)
-      .attr("stroke-width", 1e-6)
-      .remove();
-
-    this.linksSelection
-      .enter()
-      .append("line")
-      .attr("class", "link");
+      .attr("class", "node")
+      .attr("data-id", d => d.id)
+      .style("fill", "red")
+      .attr("r", 5)
+      .merge(this.nodesSelection);
 
     this.simulation.nodes(props.graph.nodes);
     this.simulation.force("link").links(props.graph.links);
     this.simulation.alpha(1).restart();
-
-    console.log("----");
   };
 
   ticked = () => {
-    console.log("tick");
-
-    this.nodesSelection.attr("transform", d => `translate(${d.x}, ${d.y})`);
-
-    this.linksSelection
-      .attr("x1", d => d.source.x)
-      .attr("y1", d => d.source.y)
-      .attr("x2", d => d.target.x)
-      .attr("y2", d => d.target.y);
+    this.nodesSelection.attr("cx", d => d.x).attr("cy", d => d.y);
   };
 
   render() {
-    console.log("Render");
-
     return (
       <div className="c-node-graph">
         <p>Graph</p>
